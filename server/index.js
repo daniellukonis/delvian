a6dbd748af4654c263d54e4d7a1861ffdb2a3e10
Daniel <daniel.lukonis@gmail.com>
2022-11-18 14:17:39 -0500

    Create baseline config

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { Server } = require('socket.io')
+const http = require('http')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+})
+
+server.listen(8080, () => {
+    console.log('server started', 8080)
+})
---
+++ todo.md
@@ -0,0 +1,35 @@
+/*  SERVER
+**  serve html, css
+**  api routes
+**  socket io
+**  track of ship, bullets, health
+**
+**
+**
+**
+**
+*/
+
+/*  CLIENT
+**  html
+**  css
+**  canvas
+**  ship entity
+**  board
+**  bullets
+**  general entity - mass, velocity, damage?, health, thrust?
+**  emit on hit
+
+entity
+
+mass
+current velocity
+desired velocity
+health
+damage
+thrust
+thrust direction
+target turn angle
+current turn angle
+angular velocity
+*/
